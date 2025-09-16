80f155a1bd6f3612d35b6eb2482a777bb9032e56
verchik <marver2525@gmail.com>
2025-09-16 18:23:51 +0700

    Приведение типов

---
+++ Pr005.java
@@ -0,0 +1,59 @@
+class Pr005 {
+	public static void main(String args[]) {
+//Составные операторы присваивания
+		//Для арифметических операций:
+		//+=	-=	*=	/=
+		int x = 0;
+		x = x+5;
+		x += 5;
+		x = x - 5;
+		x -= 5;
+		//Для логических операций:
+		//&=	|=	^=
+		
+//Преобразование типов при присваивании
+		int i;
+		float f;
+
+		i = 10;
+		f = i;
+		System.out.println("i = " + i + " f = " + f);
+		//Автоматическое преобразование типов выполняется если:
+			//целевой тип совместим с типом значения, которое ему присваивается
+			//целевой тип поддерживает более широкий диапазон допустимых значений
+
+		long L;
+		double D;
+		L = 212352634742345235L;
+		D = L;
+		System.out.println("L = " + L + " D = " + D);
+
+		//Пример недопустимого автоматического преобразования типов:
+		/*
+		D = 212352634742345235L;
+		L = D;
+		System.out.println("L = " + L + " D = " + D);
+		*/
+
+		//"Ручное" приведение одного типа к другому
+		D = 212352634742345235L;
+		L = (long)D;
+		System.out.println("L = " + L + " D = " + D);
+
+		System.out.println();
+		byte byte1 = 125;
+		char ch;
+		ch = (char) byte1;
+		System.out.println("ch: " + ch);
+
+		int integer1 = 1095;
+		ch = (char) integer1;
+		System.out.println("ch: " + ch);
+
+		ch = 'Ю';
+		integer1 = (int) ch;
+		System.out.println("integer1: " + integer1);
+
+		D = (double) ch;
+	}
+}
