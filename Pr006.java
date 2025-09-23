4ac723b4f90a570294a4e16842a770c389d0cfc8
MarinaVer2525 <113004572+MarinaVer2525@users.noreply.github.com>
2025-09-23 12:02:04 +0700

    Первый пример цикла for

--- Pr006.java
+++ Pr006.java
@@ -90,24 +90,38 @@ class Pr006 {
 			ch = 'R';
 			char ch1 = 'A';
 			switch(ch) {
-				case 'A':
-					System.out.println("Значение A внешнего оператора switch");
-						swich(ch1) {
-							case 'A':
-								System.out.println("Значение A внутреннего оператора switch");
-								break;
-						}
-						break;
-				case 'R':
-					System.out.println("Значение A внешнего оператора switch");
-						swich(ch1) {
-							case 'A':
-								System.out.println("Значение R внутреннего оператора switch");
-								break;
-						}
-						break;
-				default:
-					System.out.println("Значение по умолчанию для внешнего switch()");
+					case 'A':
+						System.out.println("Значение A внешнего оператора switch");
+							swich(ch1) {
+								case 'A':
+									System.out.println("Значение A внутреннего оператора switch");
+									break;
+							}
+							break;
+					case 'R':
+						System.out.println("Значение R внешнего оператора switch");
+							swich(ch1) {
+								case 'A':
+									System.out.println("Значение A внутреннего оператора switch");
+									break;
+							}
+							break;
+					default:
+						System.out.println("Значение по умолчанию для внешнего switch()");
+		}
+			
+			//оператор цикла for
+			System.out.println();
+			double num, sroot, rerr;
+
+			for(num = 1.0; num < 100.0; num++) {
+				sroot = Math.scrt(num);
+				System.out.println("Квадратный корень из " + num + "равен " + sroot);
+
+				//вычисление ошибки округления
+				rerr = num - (sroot * sroot);
+				System.out.println("Ошибки округления: " + rerr);
+				System.out.println("");
 			}
 	}
 }
